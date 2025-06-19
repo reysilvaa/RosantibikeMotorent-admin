59e5dcbf2a1945e7ee0f7b9ef7e5e280e481471c
reysilvaa <reynaldsilva123@gmail.com>
2025-06-19 15:43:41 +0700

    Tambah dependensi 'nextjs-toploader' dan integrasikan ke dalam layout untuk meningkatkan pengalaman pengguna dengan menambahkan efek loading yang lebih baik.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Inter } from "next/font/google";
+import NextTopLoader from 'nextjs-toploader';
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
 const inter = Inter({ subsets: ["latin"] });
@@ -22,6 +23,18 @@ export default function RootLayout({
   return (
     <html lang="id" suppressHydrationWarning>
       <body className={inter.className}>
+        <NextTopLoader 
+          color="#2563eb"
+          initialPosition={0.08}
+          crawlSpeed={200}
+          height={3}
+          crawl={true}
+          showSpinner={true}
+          easing="ease"
+          speed={200}
+          shadow="0 0 10px #2563eb,0 0 5px #2563eb"
+          zIndex={1600}
+        />
         {children}
         <Toaster />
       </body>
